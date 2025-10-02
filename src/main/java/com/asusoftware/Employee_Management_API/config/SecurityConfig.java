d8b720c4b5af96b1a8f7aea40c8292e10e28490d
antonio <antonio.popa.dev@gmail.com>
2025-10-02 10:39:36 +0300

    Initial commit

--- src/main/java/com/asusoftware/Employee_Management_API/config/SecurityConfig.java
+++ src/main/java/com/asusoftware/Employee_Management_API/config/SecurityConfig.java
@@ -3,13 +3,13 @@ package com.asusoftware.Employee_Management_API.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true) // <<— IMPORTANT
